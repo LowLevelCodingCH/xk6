0e4e6d570a2206ab1c497442b67e0d380ec5d7c9
LowLevelCodingCH <135059709+LowLevelCodingCH@users.noreply.github.com>
2025-03-04 12:19:44 +0100

    Update syscalls.h

--- src/cfiles/litrix/syscalls.h
+++ src/cfiles/litrix/syscalls.h
@@ -1,5 +1,4 @@
-void XK_Prefix(GetRegs)(regs_t* regs)
-{
+void XK_Prefix(GetRegs)(regs_t* regs) {
 //    __asm__ volatile (
 //        "mov %%eax, %0\n\t"
 //        "mov %%edx, %1\n\t"
@@ -16,8 +15,7 @@ void XK_Prefix(GetRegs)(regs_t* regs)
 void XK_Prefix(RunSyscall)(uint32_t index,
                            uint32_t arg0   /* if diskio (READ/WRITE) or read or write */,
                            uint32_t arg1 /* if diskio (READ/WRITE) or read or write */,
-                           uint32_t arg2 /* if diskio (READ/WRITE) or read */)
-{
+                           uint32_t arg2 /* if diskio (READ/WRITE) or read */) {
     switch(index) {
         case SYS_WRITE:      kprintbuf((char*)arg0, arg1);             break;
         case SYS_READ:       read_word((char*)arg0, arg1, arg2, arg1); break;
@@ -26,8 +24,7 @@ void XK_Prefix(RunSyscall)(uint32_t index,
     }
 }
 
-void XK_Prefix(RunRegSyscall)(regs_t *regs)
-{
+void XK_Prefix(RunRegSyscall)(regs_t *regs) {
     XK_Prefix(RunSyscall)(regs->eax,
                           regs->ebx,
                           regs->ecx,
