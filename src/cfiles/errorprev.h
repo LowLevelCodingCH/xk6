b23f3ba200d17376ca4a398f570a28b66f0e04fa
LowLevelCodingCH <135059709+LowLevelCodingCH@users.noreply.github.com>
2025-03-08 14:08:11 +0100

    Delete src directory

--- src/cfiles/errorprev.h
+++
@@ -1,6 +0,0 @@
-int __stack_chk_fail_local = 0;
-int _stack_chk_fail_local  = 0;
-int __GLOBAL_OFFSET_TABLE_ = 0;
-int __GLOBAL_OFFSET_TABLE  = 0;
-int _GLOBAL_OFFSET_TABLE   = 0;
-int _GLOBAL_OFFSET_TABLE_  = 0;
--- src/cfiles/settings.h
+++
@@ -1,9 +0,0 @@
-//#define __Kernel_Print_Log_Then_Halt__
-
-//#define CoolLoadingBar
-#define CoolLoadingBarDelay   3000
-#define CoolLoadingBarDelayFF 1500
-
-#define PAGESIZE       4096
-#define PAGEAMOUNT     1024
-#define MAX_TASK_AMOUNT 16
